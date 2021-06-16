71d96ea80b6e26b580b8b62fb316be2404c544a2
zjun <565061763@qq.com>
2021-06-16 23:25:30 +0800

    test

--- app/src/androidTest/java/com/sd/demo/adapter/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/sd/demo/adapter/ExampleInstrumentedTest.java
@@ -20,18 +20,20 @@ public class ExampleInstrumentedTest {
         final List<TestModel> listData = TestModel.get(2);
         dataHolder.setData(listData);
 
+        // test setData
         Assert.assertEquals(2, dataHolder.size());
         Assert.assertEquals("0", dataHolder.get(0).name);
         Assert.assertEquals("1", dataHolder.get(1).name);
 
+        // test removeData
         Assert.assertEquals("1", dataHolder.removeData(1).name);
         Assert.assertEquals(true, dataHolder.removeData(new TestModel("0")));
         Assert.assertEquals(0, dataHolder.size());
 
+        // test addData
         dataHolder.addData(new TestModel("a"));
         dataHolder.addData(new TestModel("b"));
         dataHolder.addData(1, new TestModel("c"));
-
         Assert.assertEquals(3, dataHolder.size());
         Assert.assertEquals("a", dataHolder.get(0).name);
         Assert.assertEquals("c", dataHolder.get(1).name);
