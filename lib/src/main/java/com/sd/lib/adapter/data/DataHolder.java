ea721b9500f22be5115f09173c2e371699b36367
zjun <565061763@qq.com>
2021-06-16 18:20:51 +0800

    format

--- lib/src/main/java/com/sd/lib/adapter/data/DataHolder.java
+++ lib/src/main/java/com/sd/lib/adapter/data/DataHolder.java
@@ -6,22 +6,16 @@ public interface DataHolder<T>
 {
     /**
      * 添加数据变化回调
-     *
-     * @param callback
      */
     void addDataChangeCallback(DataChangeCallback<T> callback);
 
     /**
      * 移除数据变化回调
-     *
-     * @param callback
      */
     void removeDataChangeCallback(DataChangeCallback<T> callback);
 
     /**
      * 设置数据转换器
-     *
-     * @param transform
      */
     void setDataTransform(DataTransform<T> transform);
 
@@ -29,65 +23,41 @@ public interface DataHolder<T>
 
     /**
      * 设置数据集
-     *
-     * @param list
      */
     void setData(List<? extends T> list);
 
     /**
      * 在末尾添加数据
-     *
-     * @param data
-     * @return
      */
     boolean addData(T data);
 
     /**
      * 在末尾添加数据
-     *
-     * @param list
-     * @return
      */
     boolean addData(List<? extends T> list);
 
     /**
      * 在index位置添加数据
-     *
-     * @param index
-     * @param data
      */
     void addData(int index, T data);
 
     /**
      * 在index位置添加数据
-     *
-     * @param index
-     * @param list
-     * @return
      */
     boolean addData(int index, List<? extends T> list);
 
     /**
      * 移除数据
-     *
-     * @param data
-     * @return
      */
     boolean removeData(T data);
 
     /**
      * 移除index位置的数据
-     *
-     * @param index
-     * @return
      */
     T removeData(int index);
 
     /**
      * 更新index位置的数据
-     *
-     * @param index
-     * @param data
      */
     void updateData(int index, T data);
 
@@ -95,92 +65,63 @@ public interface DataHolder<T>
 
     /**
      * 位置在数据集中是否合法
-     *
-     * @param index
-     * @return
      */
     boolean isIndexLegal(int index);
 
     /**
      * 返回index位置的数据
-     *
-     * @param index
-     * @return
      */
     T get(int index);
 
     /**
      * 获得数据数量
-     *
-     * @return
      */
     int size();
 
     /**
      * 实体在数据集中的位置
-     *
-     * @param data
-     * @return
      */
     int indexOf(Object data);
 
     /**
      * 获得数据集
-     *
-     * @return
      */
     List<T> getData();
 
     /**
      * 数据变化回调
-     *
-     * @param <T>
      */
     interface DataChangeCallback<T>
     {
         /**
          * 数据集发生变化
-         *
-         * @param list
          */
         void onDataChanged(List<T> list);
 
         /**
          * index位置的数据发生变化
-         *
-         * @param index
-         * @param data
          */
         void onDataChanged(int index, T data);
 
         /**
          * index位置添加了数据
-         *
-         * @param index
-         * @param list
          */
         void onDataAdded(int index, List<T> list);
 
         /**
          * index位置的数据被删除了
-         *
-         * @param index
-         * @param data
          */
         void onDataRemoved(int index, T data);
     }
 
     /**
      * 数据转换器
-     *
-     * @param <T>
      */
     interface DataTransform<T>
     {
         /**
          * 转换数据
          *
-         * @param source
          * @return null-不进行转换
          */
         T transform(T source);
