3e922ed01d5fbd1973bbbd5e7c07b4e28e50701c
zjun <565061763@qq.com>
2021-06-16 21:27:44 +0800

    add method

--- lib/src/main/java/com/sd/lib/adapter/data/DataHolder.java
+++ lib/src/main/java/com/sd/lib/adapter/data/DataHolder.java
@@ -2,8 +2,7 @@ package com.sd.lib.adapter.data;
 
 import java.util.List;
 
-public interface DataHolder<T>
-{
+public interface DataHolder<T> {
     /**
      * 添加数据变化回调
      */
@@ -29,32 +28,32 @@ public interface DataHolder<T>
     /**
      * 在末尾添加数据
      */
-    boolean addData(T data);
+    boolean add(T data);
+
+    /**
+     * 在index位置添加数据
+     */
+    void add(int index, T data);
 
     /**
      * 在末尾添加数据
      */
-    boolean addData(List<? extends T> list);
+    boolean addAll(List<? extends T> list);
 
     /**
      * 在index位置添加数据
      */
-    void addData(int index, T data);
-
-    /**
-     * 在index位置添加数据
-     */
-    boolean addData(int index, List<? extends T> list);
+    boolean addAll(int index, List<? extends T> list);
 
     /**
      * 移除数据
      */
-    boolean removeData(T data);
+    boolean remove(T data);
 
     /**
      * 移除index位置的数据
      */
-    T removeData(int index);
+    T removeAt(int index);
 
     /**
      * 更新index位置的数据
@@ -89,10 +88,45 @@ public interface DataHolder<T>
     List<T> getData();
 
     /**
+     * {@link #add(Object)}
+     */
+    @Deprecated
+    boolean addData(T data);
+
+    /**
+     * {@link #add(int, Object)}
+     */
+    @Deprecated
+    void addData(int index, T data);
+
+    /**
+     * {@link #addAll(List)}
+     */
+    @Deprecated
+    boolean addData(List<? extends T> list);
+
+    /**
+     * {@link #addAll(int, List)}
+     */
+    @Deprecated
+    boolean addData(int index, List<? extends T> list);
+
+    /**
+     * {@link #remove(Object)}
+     */
+    @Deprecated
+    boolean removeData(T data);
+
+    /**
+     * {@link #removeAt(int)}
+     */
+    @Deprecated
+    T removeData(int index);
+
+    /**
      * 数据变化回调
      */
-    interface DataChangeCallback<T>
-    {
+    interface DataChangeCallback<T> {
         /**
          * 数据集发生变化
          */
@@ -117,8 +151,7 @@ public interface DataHolder<T>
     /**
      * 数据转换器
      */
-    interface DataTransform<T>
-    {
+    interface DataTransform<T> {
         /**
          * 转换数据
          *
--- lib/src/main/java/com/sd/lib/adapter/data/ListDataHolder.java
+++ lib/src/main/java/com/sd/lib/adapter/data/ListDataHolder.java
@@ -4,64 +4,56 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.ListIterator;
 
-public class ListDataHolder<T> implements DataHolder<T>
-{
+public class ListDataHolder<T> implements DataHolder<T> {
     private List<T> mListData = new ArrayList<>();
     private final List<DataChangeCallback<T>> mListDataChangeCallback = new ArrayList<>();
     private DataTransform<T> mDataTransform;
 
     @Override
-    public void addDataChangeCallback(DataChangeCallback<T> callback)
-    {
-        if (callback == null || mListDataChangeCallback.contains(callback))
+    public void addDataChangeCallback(DataChangeCallback<T> callback) {
+        if (callback == null || mListDataChangeCallback.contains(callback)) {
             return;
+        }
 
         mListDataChangeCallback.add(callback);
     }
 
     @Override
-    public void removeDataChangeCallback(DataChangeCallback<T> callback)
-    {
+    public void removeDataChangeCallback(DataChangeCallback<T> callback) {
         mListDataChangeCallback.remove(callback);
     }
 
     @Override
-    public void setDataTransform(DataTransform<T> dataTransform)
-    {
+    public void setDataTransform(DataTransform<T> dataTransform) {
         mDataTransform = dataTransform;
     }
 
-    private ListIterator<DataChangeCallback<T>> getListIteratorPrevious()
-    {
+    private ListIterator<DataChangeCallback<T>> getListIteratorPrevious() {
         return mListDataChangeCallback.listIterator(mListDataChangeCallback.size());
     }
 
     //---------- modify start ----------
 
     @Override
-    public void setData(List<? extends T> list)
-    {
-        if (list != null)
-        {
+    public void setData(List<? extends T> list) {
+        if (list != null) {
             mListData = transformData(list);
-        } else
-        {
+        } else {
             mListData = new ArrayList<>();
         }
 
         final List<T> listCopy = new ArrayList<>(mListData);
         final ListIterator<DataChangeCallback<T>> it = getListIteratorPrevious();
-        while (it.hasPrevious())
-        {
+        while (it.hasPrevious()) {
             it.previous().onDataChanged(listCopy);
         }
     }
 
     @Override
-    public boolean addData(T data)
-    {
-        if (data == null)
+    public boolean add(T data) {
+        if (data == null) {
             return false;
+        }
 
         data = transformData(data);
 
@@ -72,40 +64,17 @@ public class ListDataHolder<T> implements DataHolder<T>
         listCopy.add(data);
 
         final ListIterator<DataChangeCallback<T>> it = getListIteratorPrevious();
-        while (it.hasPrevious())
-        {
+        while (it.hasPrevious()) {
             it.previous().onDataAdded(index, listCopy);
         }
-
         return result;
     }
 
     @Override
-    public boolean addData(List<? extends T> list)
-    {
-        if (list == null || list.isEmpty())
-            return false;
-
-        list = transformData(list);
-
-        final int index = size();
-        final boolean result = mListData.addAll(list);
-
-        final List<T> listCopy = new ArrayList<>(list);
-        final ListIterator<DataChangeCallback<T>> it = getListIteratorPrevious();
-        while (it.hasPrevious())
-        {
-            it.previous().onDataAdded(index, listCopy);
-        }
-
-        return result;
-    }
-
-    @Override
-    public void addData(int index, T data)
-    {
-        if (data == null)
+    public void add(int index, T data) {
+        if (data == null) {
             return;
+        }
 
         data = transformData(data);
         mListData.add(index, data);
@@ -114,66 +83,79 @@ public class ListDataHolder<T> implements DataHolder<T>
         list.add(data);
 
         final ListIterator<DataChangeCallback<T>> it = getListIteratorPrevious();
-        while (it.hasPrevious())
-        {
+        while (it.hasPrevious()) {
             it.previous().onDataAdded(index, list);
         }
     }
 
     @Override
-    public boolean addData(int index, List<? extends T> list)
-    {
-        if (list == null || list.isEmpty())
+    public boolean addAll(List<? extends T> list) {
+        if (list == null || list.isEmpty()) {
             return false;
+        }
+
+        list = transformData(list);
+
+        final int index = size();
+        final boolean result = mListData.addAll(list);
+
+        final List<T> listCopy = new ArrayList<>(list);
+        final ListIterator<DataChangeCallback<T>> it = getListIteratorPrevious();
+        while (it.hasPrevious()) {
+            it.previous().onDataAdded(index, listCopy);
+        }
+        return result;
+    }
+
+    @Override
+    public boolean addAll(int index, List<? extends T> list) {
+        if (list == null || list.isEmpty()) {
+            return false;
+        }
 
         list = transformData(list);
         final boolean result = mListData.addAll(index, list);
 
         final List<T> listCopy = new ArrayList<>(list);
         final ListIterator<DataChangeCallback<T>> it = getListIteratorPrevious();
-        while (it.hasPrevious())
-        {
+        while (it.hasPrevious()) {
             it.previous().onDataAdded(index, listCopy);
         }
-
         return result;
     }
 
     @Override
-    public boolean removeData(T data)
-    {
+    public boolean remove(T data) {
         final int position = indexOf(data);
-        return removeData(position) != null;
+        return removeAt(position) != null;
     }
 
     @Override
-    public T removeData(int index)
-    {
-        if (!isIndexLegal(index))
+    public T removeAt(int index) {
+        if (!isIndexLegal(index)) {
             return null;
+        }
 
         final T model = mListData.remove(index);
 
         final ListIterator<DataChangeCallback<T>> it = getListIteratorPrevious();
-        while (it.hasPrevious())
-        {
+        while (it.hasPrevious()) {
             it.previous().onDataRemoved(index, model);
         }
         return model;
     }
 
     @Override
-    public void updateData(int index, T data)
-    {
-        if (data == null || !isIndexLegal(index))
+    public void updateData(int index, T data) {
+        if (data == null || !isIndexLegal(index)) {
             return;
+        }
 
         data = transformData(data);
         mListData.set(index, data);
 
         final ListIterator<DataChangeCallback<T>> it = getListIteratorPrevious();
-        while (it.hasPrevious())
-        {
+        while (it.hasPrevious()) {
             it.previous().onDataChanged(index, data);
         }
     }
@@ -181,74 +163,102 @@ public class ListDataHolder<T> implements DataHolder<T>
     //---------- modify end ----------
 
     @Override
-    public boolean isIndexLegal(int index)
-    {
+    public boolean isIndexLegal(int index) {
         return index >= 0 && index < size();
     }
 
     @Override
-    public T get(int index)
-    {
-        if (isIndexLegal(index))
+    public T get(int index) {
+        if (isIndexLegal(index)) {
             return mListData.get(index);
-        else
+        } else {
             return null;
+        }
     }
 
     @Override
-    public int size()
-    {
+    public int size() {
         return mListData.size();
     }
 
     @Override
-    public int indexOf(Object data)
-    {
-        if (data == null || mListData.isEmpty())
+    public int indexOf(Object data) {
+        if (data == null || mListData.isEmpty()) {
             return -1;
+        }
 
         final List<T> list = new ArrayList<>(mListData);
-        for (int i = 0; i < list.size(); i++)
-        {
+        for (int i = 0; i < list.size(); i++) {
             final T item = list.get(i);
-            if (item != null && item.equals(data))
+            if (item != null && item.equals(data)) {
                 return i;
+            }
         }
         return -1;
     }
 
     @Override
-    public List<T> getData()
-    {
+    public List<T> getData() {
         return mListData;
     }
 
-    private List<T> transformData(List<? extends T> data)
-    {
-        if (mDataTransform == null)
+    private List<T> transformData(List<? extends T> data) {
+        if (mDataTransform == null) {
             return (List<T>) data;
+        }
 
-        if (data == null || data.isEmpty())
+        if (data == null || data.isEmpty()) {
             return (List<T>) data;
+        }
 
         final List<T> listResult = new ArrayList<>(data.size());
-        for (T item : data)
-        {
+        for (T item : data) {
             final T transform = transformData(item);
             listResult.add(transform);
         }
         return listResult;
     }
 
-    private T transformData(T data)
-    {
-        if (mDataTransform == null)
+    private T transformData(T data) {
+        if (mDataTransform == null) {
             return data;
+        }
 
         final T transform = mDataTransform.transform(data);
-        if (transform == null)
+        if (transform == null) {
             return data;
+        }
 
         return transform;
+    }
+
+    @Override
+    public boolean addData(T data) {
+        return add(data);
+    }
+
+    @Override
+    public void addData(int index, T data) {
+        add(index, data);
+    }
+
+    @Override
+    public boolean addData(List<? extends T> list) {
+        return addAll(list);
+    }
+
+    @Override
+    public boolean addData(int index, List<? extends T> list) {
+        return addAll(index, list);
+    }
+
+    @Override
+    public boolean removeData(T data) {
+        return remove(data);
+    }
+
+    @Override
+    public T removeData(int index) {
+        return removeAt(index);
     }
 }
